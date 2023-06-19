d0609532ee2d3c07b7a36e28d184ccacdffb27f7
Bogdan <bogd.moskv@gmail.com>
2023-06-19 12:21:45 +0300

    UserController: add some action methods

---
+++ MaterApp/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaterApp.Controllers
+{
+    [ApiController]
+    [Route("users")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            var user = _context.Users.Find(id);
+
+            if (user== null)
+            {
+                return NotFound("We have no user with such id!");
+            }
+            return Ok(user);
+        }
+    }
+}
